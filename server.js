ed78b0ae3516f3861eea09e652f44c4f64a6ad36
Shubhangi Rastogi <rastogishubhangi137@gmail.com>
2018-01-24 18:11:57 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/public'));
+app.set('view engine','hbs')
+app.use((req, res,next) =>{
+  var now = new Date().toString();
+  var log =`${now}: ${req.method}, ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n',(err) =>{ if(err)
+    {console.log('unable to log it in the files');}
+  });
+  next();
+});
+
+// app.use((req,res,next)=> {
+//   res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getcurrentyear', () =>{
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamit',(text) =>{
+  return text.toUpperCase();
+});
+
+
+
+app.get('/',(req,res) =>{
+  res.render('home.hbs',{
+    pagetitle:'home page',
+    message:'welcome to my first web page'
+
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pagetitle: 'About page'
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errormessage: 'unable to connect to the net server'});
+});
+
+
+app.listen(3000,() => {console.log('port 3000 is set up');});
